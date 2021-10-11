8a80751391de8c4824fd3f32fe8b832ff6c5d369
MaxwellHouston <87088784+MaxwellHouston@users.noreply.github.com>
2021-10-11 11:37:15 -0400

    added local state and functionality for the e topics checkbox

--- src/features/topics/Topics.js
+++ src/features/topics/Topics.js
@@ -1,31 +1,49 @@
 import React from "react";
+import { useState } from "react";
 
 
 
 export const Topics = (props) => {
 
+    const [topics, setTopics] = useState([])
+
+    const toggleTopic = ({ target }) => {
+        if(target.checked) {
+            setTopics( prev => [...prev, target.value])
+        } else if (!target.checked){
+            let newTopics = topics.filter(topic => topic !== target.value);
+            setTopics(newTopics)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target)
+    }
+
 
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>Choose your content</h2>
             <label for="news">News</label>
-            <input type="checkbox" id="news" value="news"/>
+            <input onClick={toggleTopic} type="checkbox" id="news" value="news"/>
             <label for="tech">Tech</label>
-            <input type="checkbox" id="tech" value="tech"/>
+            <input onClick={toggleTopic} type="checkbox" id="tech" value="tech"/>
             <label for="travel">Travel</label>
-            <input type="checkbox" id="travel" value="travel"/>
+            <input onClick={toggleTopic} type="checkbox" id="travel" value="travel"/>
             <label for="fashion">Fashion</label>
-            <input type="checkbox" id="fashion" value="fashion"/>
+            <input onClick={toggleTopic} type="checkbox" id="fashion" value="fashion"/>
             <label for="humor">Humor</label>
-            <input type="checkbox" id="humor" value="humor"/>
+            <input onClick={toggleTopic} type="checkbox" id="humor" value="humor"/>
             <label for="gaming">Gaming</label>
-            <input type="checkbox" id="gaming" value="gaming"/>
+            <input onClick={toggleTopic} type="checkbox" id="gaming" value="gaming"/>
             <label for="cars">Cars</label>
-            <input type="checkbox" id="cars" value="cars"/>
+            <input onClick={toggleTopic} type="checkbox" id="cars" value="cars"/>
             <label for="animals">Animals</label>
-            <input type="checkbox" id="animals" value="animals"/>
+            <input onClick={toggleTopic} type="checkbox" id="animals" value="animals"/>
             <label for="sports">Sports</label>
-            <input type="checkbox" id="sports" value="sports"/>
+            <input onClick={toggleTopic} type="checkbox" id="sports" value="sports"/>
+            <input type="submit" value="submit" />
         </form>
     )
 }
